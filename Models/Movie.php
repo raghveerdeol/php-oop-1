dc39d7b7f7c7106953fe0bc9f9f8f070b318c288
raghveerdeol <deolraghveer5@gmail.com>
2024-07-02 20:46:18 +0200

    aggiunto constract nella classe movie

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,9 +1,19 @@
 <?php 
 class movie {
+    // variabili istanza 
     public $titolo;
     public $titolo_originale;
     public $data_uscita;
     public $stelle;
     public $nazione_di_produzione;
+
+    // costrutore 
+    public function __construct($_titolo, $_titolo_originale, $_data_uscita, $_stelle, $_nazione_di_produzione){
+        $this->titolo = $_titolo;
+        $this->titolo_originale = $_titolo_originale;
+        $this->data_uscita = $_data_uscita;
+        $this->stelle = $_stelle;
+        $this->nazione_di_produzione = $_nazione_di_produzione;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 include_once __DIR__ . "/Models/Movie.php";
 
-$terminetor = new movie();
-$terminetor->titolo="terminetor";
-$terminetor->stelle=5;
+$terminetor = new movie("terminetor", "terminetor", "01-01-2000", "4", "Usa");
+// $terminetor->titolo="terminetor";
+// $terminetor->stelle=5;
 var_dump($terminetor)
 ?>
